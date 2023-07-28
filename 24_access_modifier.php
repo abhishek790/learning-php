71477198f2cb3d642a2f1da922a8d655013cb3e1
Abhishek Mishra <mishraavi790@gmail.com>
2023-07-28 18:57:43 +0545

    modifier

--- 24_access_modifier.php
+++ 24_access_modifier.php
@@ -1,4 +1,23 @@
 <?php
 
+class Database
+{
+    // when no access modifier is used it uses default i.e. public, 
+    //The var keyword in PHP is used to declare a property or variable of class which is public by default. The var keyword is same as public when declaring variables or property of a class.
+    var $name = "Avsek";
+
+    // by default methods are public
+    function showName()
+    {
+        echo "<br> $this->name";
+    }
+
+}
+
+$avsek = new Database();
+echo $avsek->name;
+$avsek->showName();
+
+
 
 ?>
