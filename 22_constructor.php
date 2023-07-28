53d4d9b1cc1cbedc06b7f749f09d29fb4296e9f1
Abhishek Mishra <mishraavi790@gmail.com>
2023-07-28 18:37:40 +0545

    constuctor

--- 22_constructor.php
+++ 22_constructor.php
@@ -4,8 +4,24 @@ class Employee
     // class properties
     public $name;
     public $salary;
+    /*
+        //class methods
+        //Constructor = It allows you to initialize objects. It is the code which is executed whenever a new object is instantiated
+        function __construct()
+        {
+            echo "this is my contructor for employee";
+        }
+    */
+    function __construct($name, $salary)
+    {
+        $this->name = $name;
+        $this->salary = $salary;
 
-    //class methods
-    //Constructor = It allows you to initialize objects. It is the code which is executed whenever a new object is instantiated
+    }
 }
+
+$rohan = new Employee("ron", 150000);
+$jon = new Employee('jon', 200000);
+
+echo "salary of rohan is  $jon->salary ";
 ?>
